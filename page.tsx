b7406dce9020e1ddc1c143324d911f9b74164ef8
Muhammad Shoaib <136969366+shoby056@users.noreply.github.com>
2024-09-30 13:19:03 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,28 @@
+export default function home(){
+  return(
+  
+
+    <><header>
+     
+
+      <ul>
+      <li className="items">Profile </li>
+        <li className="items">Home </li>
+        <li className="items">About Us </li>
+        <li className="items">Contact Us</li>
+
+      </ul>
+
+
+
+    </header>
+    <div className="container">
+
+
+        <div className="left-container">Hi
+          ,<br />Iam Muhammad Shoaib Shaikh and Iam Currently  Web developer at Giaic and Junior software Engineer  </div>
+        <div className="right-container"><img src="https://avatars.githubusercontent.com/u/136969366?v=4" alt="image" className="img" /></div>
+      </div></>
+
+  )
+}
